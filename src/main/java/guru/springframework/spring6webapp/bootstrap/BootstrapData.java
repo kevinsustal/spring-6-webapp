b4b8fec88a8528dd666726ac7ccf9e84d1c7402d
kevinsustal <kevinsustal@gmail.com>
2023-07-26 22:47:26 +0800

    Adding publisher entity as assignment

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +14,29 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        //Ading Publisher
+        Publisher macmillan = new Publisher();
+        macmillan.setPublisherName("Macmillan Publishers");
+        macmillan.setAddress("Calamba, Philippines");
+        macmillan.setCity("Calamba");
+        macmillan.setState("Laguna");
+        macmillan.setZip("4027");
+
+        Publisher macmillanSaved = publisherRepository.save(macmillan);
+        publisherRepository.save(macmillanSaved);
+
+        //Adding first author
         Author kevin = new Author();
         kevin.setFirstName("Kevin");
         kevin.setLastName("Sustal");
@@ -32,10 +48,13 @@ public class BootstrapData implements CommandLineRunner {
         Author kevinSaved = authorRepository.save(kevin);
         Book addSaved = bookRepository.save(add);
 
+
+        //Adding second Author
         Author nicole = new Author();
         nicole.setFirstName("Nicole");
         nicole.setLastName("Sustal");
 
+        //Adding books
         Book noEJB = new Book();
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("783453453");
@@ -52,6 +71,7 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
     }
 }
